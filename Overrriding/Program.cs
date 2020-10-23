d5dfddb5e4f4bd67a89bb95549757717e998896c
teechamp <temidayosamuelbambo@gmail.com>
2020-10-23 07:22:58 +0100

    first commit

---
+++ Overrriding/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Overrriding
+{   
+                class Class1
+            {
+                public virtual void show()
+                {
+                    Console.WriteLine("Super class show method");
+                }
+            }
+     class Class2 : Class1
+            {
+                public override void show()
+                {
+                    Console.WriteLine("Sub class override show method");
+                }
+            }
+
+    
+            class Program
+                {
+            
+            static void Main(string[] args)
+                {
+                    Class2 obj = new Class2();
+                    obj.show();
+                    Console.ReadKey();
+               }
+         
+    }   
+}
